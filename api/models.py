60be23180cf0c45c63bccfc1354c85d0aa218eab
janodin <japatundog@usm.edu.ph>
2022-08-01 23:39:31 +0800

    Abstract user

--- api/models.py
+++ api/models.py
@@ -1,15 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-
-class User(models.Model):
-    first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+class User(AbstractUser):
+    username = models.CharField(max_length=200, unique=True)
+    email = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.username
 
 
 class Customer(models.Model):
